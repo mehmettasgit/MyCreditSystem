b135c222c1d639628500b5af9fa75422c546ea76
mehmettasgit <mehmet.tsgt@gmail.com>
2025-04-27 21:33:18 +0300

    Springboot actuator

--- src/main/java/com/creditsystem/config/SecurtiyConfig.java
+++ src/main/java/com/creditsystem/config/SecurtiyConfig.java
@@ -1,27 +1,32 @@
 package com.creditsystem.config;
 
 
+import com.creditsystem.components.JwtAuthenticationFilter;
 import com.creditsystem.service.CustomUserDetailsService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
+@EnableMethodSecurity(prePostEnabled = true)
 public class SecurtiyConfig {
 
-    @Bean
-    public PasswordEncoder passwordEncoder(){
-        return new BCryptPasswordEncoder();
-    }
+
+    @Autowired
+    private JwtAuthenticationFilter jwtAuthenticationFilter;
 
 
     @Autowired
@@ -29,19 +34,25 @@ public class SecurtiyConfig {
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http
-                .csrf().disable()
-                .authenticationProvider(authProvider())
-                .authorizeHttpRequests(authorize -> authorize
-                        .antMatchers("/public").permitAll()
-                        .antMatchers("/api/roles/create").permitAll()
-                        .antMatchers("/api/users/create").permitAll()
-                        .antMatchers("/api/credit").permitAll()
-                        .antMatchers("/api/credit/**").permitAll()
-                        .anyRequest().authenticated()
+        return ((org.springframework.security.config.annotation.web.builders.HttpSecurity) http)
+                .csrf(csrf -> csrf.disable()) // CSRF'yi devre dışı bırakıyoruz (REST için genelde)
+                .authorizeHttpRequests(auth -> auth
+                        /*.antMatchers(HttpMethod.POST, "/api/auth/login").permitAll()
+                        .antMatchers(HttpMethod.POST, "/api/users/create").permitAll()
+                        .antMatchers(HttpMethod.GET, "/api/**").hasAnyRole("ADMIN", "USER", "MODERATOR")
+                        .antMatchers(HttpMethod.POST, "/api/**").hasAnyRole("ADMIN", "USER")
+                        .antMatchers(HttpMethod.PUT, "/api/**").hasRole("ADMIN")
+                        .antMatchers(HttpMethod.DELETE, "/api/**").hasRole("ADMIN")
+                        .antMatchers("/api/auth/**").permitAll() // login, register herkese açık
+                        .anyRequest().authenticated()*/
+                        .antMatchers("/actuator/**").permitAll()
+                        .anyRequest().permitAll()
+
                 )
-                .httpBasic();
-        return http.build();
+                .sessionManagement(sess -> sess.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                // Filtreyi ekle
+                .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class)
+                .build();
     }
 
     @Bean
@@ -50,5 +61,17 @@ public class SecurtiyConfig {
         authProvider.setUserDetailsService(customUserDetailsService);
         authProvider.setPasswordEncoder(passwordEncoder());
         return authProvider;
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public org.springframework.security.authentication.AuthenticationManager
+    authenticationManager(org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration configuration)
+            throws Exception {
+        return configuration.getAuthenticationManager();
     }
 }
